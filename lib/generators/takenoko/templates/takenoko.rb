befceb3750f0b7a14a5353ea5c09f141a3b517f9
Khiem Nguyen <khiem-nguyen@kayac.com>
2016-09-06 18:03:48 +0900

    dev auto generate config from spreadsheet

--- lib/generators/takenoko/templates/takenoko.rb
+++ lib/generators/takenoko/templates/takenoko.rb
@@ -1,4 +1,27 @@
 Takenoko.config do |conf|
+
+  #Global Spreadsheet id, optain via url of your spreadsheet
+  # conf.sheet_id = "SPREADSHEET_ID"
+
+  # Path to service account cridential file when using account service permission
+  # If not set, Takenoko will use peronal cridential auto matically and as you for permission at the first time via command line
   # conf.google_cridential_file = "path to google credential file.json"
+
+  # Path to mapping file, if not be set, Takenoko will fetch all worksheet and export base on worksheet
   # conf.mapping_file = "path to mapping file.yml"
+
+
+  # GLOBAL CONFIG, you can overwrite under config for each table in mapping_file.yml
+  # Export file format, default :csv, support: csv,yaml,json
+  # conf.file_extension = :csv
+
+  # Truncate all data before saving, default: false
+  # conf.truncate_all_data = false
+
+  # Export file location, default: db/spreadsheet
+  # conf.export_file_location = "db/spreadsheet"
+
+  # Allow overwrite duplicated row, default: true
+  # conf.allow_overwrite = true
+
 end
--- lib/takenoko/google_client.rb
+++ lib/takenoko/google_client.rb
@@ -1,14 +1,27 @@
 module Takenoko
   class GoogleClient
-    def initialize(cridential_file)
-      @cridential = JSON.parse(File.read(cridential_file)).with_indifferent_access
+    def initialize(cridential_file=nil)
+      if cridential_file && ::File.exist?(cridential_file)
+        @cridential = JSON.parse(File.read(cridential_file)).with_indifferent_access
+      end
     end
 
     def get_table(table_name)
       table = Takenoko.table_config(table_name)
       raise "GoogleDrive: Sheet not found" unless sheet = session.spreadsheet_by_key(table['sheet_id'])
-      raise "GoogleDrive: Worksheet not found: worksheet_id #{table['worksheet_id']}" unless worksheet = sheet.worksheet_by_gid(table['worksheet_id'])
-      header = worksheet.header.select {|h| table[:columns_mapping].keys.include?(h)}
+
+      if table[:worksheet_id].present?
+        worksheet = sheet.worksheet_by_gid(table[:worksheet_id])
+        table[:worksheet] = worksheet.title
+      elsif table[:worksheet].present?
+        raise "Worksheet #{table[:worksheet]} not found" unless worksheet = sheet.worksheet_by_title(table[:worksheet])
+        table[:worksheet_id] = worksheet.gid.to_i
+      elsif
+        raise "You must specify worksheet or worksheet_id if mapping_file.yml"
+      end
+
+      update_table_config(table,worksheet.header)
+
       rows = worksheet.populated_rows.map do |r|
         hash = HashWithIndifferentAccess.new
         table['columns_mapping'].each do |key,val|
@@ -29,21 +42,53 @@ module Takenoko
     end
 
     def session
-    key = OpenSSL::PKey::RSA.new(@cridential['private_key'])
-    auth = Signet::OAuth2::Client.new(
-      token_credential_uri: @cridential['token_uri'],
-      audience: @cridential['token_uri'],
-      scope: %w(
-        https://www.googleapis.com/auth/drive
-        https://spreadsheets.google.com/feeds/
-      ),
-      issuer: @cridential['client_email'],
-      signing_key: key
-    )
+      unless @cridential
+        return GoogleDrive.saved_session(Takenoko.personal_cridential_file)
+      end
 
-    auth.fetch_access_token!
-    session = GoogleDrive.login_with_oauth(auth.access_token)
-    return session
+      key = OpenSSL::PKey::RSA.new(@cridential['private_key'])
+      auth = Signet::OAuth2::Client.new(
+        token_credential_uri: @cridential['token_uri'],
+        audience: @cridential['token_uri'],
+        scope: %w(
+          https://www.googleapis.com/auth/drive
+          https://spreadsheets.google.com/feeds/
+        ),
+        issuer: @cridential['client_email'],
+        signing_key: key
+      )
+
+      auth.fetch_access_token!
+      session = GoogleDrive.login_with_oauth(auth.access_token)
+      return session
+    end
+
+    def spreadsheet(sheet_id=Takenoko.sheet_id)
+      session.spreadsheet_by_key(sheet_id)
+    end
+
+    private
+    def update_table_config(table,ws_header)
+      ws_header = ws_header.select do |col|
+        col.present? && ! col.match(/\s*#.*/)
+      end
+
+      if(table[:columns_mapping].blank?)
+        columns_mapping = {}
+        ws_header.each do |col| columns_mapping[col.to_sym] = col end
+        table[:columns_mapping] = columns_mapping
+      end
+
+      unless table[:columns_mapping].key?(table[:find_column])
+          table[:columns_mapping][table[:find_column]] = nil 
+      end
+
+      table[:columns_mapping].each do |s_col,db_col|
+        table[:columns_mapping][s_col] = s_col unless db_col 
+      end
+
+      table[:header] = table[:columns_mapping].keys
+      return table
     end
   end
 end
