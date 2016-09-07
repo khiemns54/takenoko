923b0ff2138076cba707374b0fc2896e08163dfb
Khiem Nguyen <khiem-nguyen@kayac.com>
2016-09-07 20:07:22 +0900

    refactor code, move mapping generator to seperated module

--- lib/takenoko.rb
+++ lib/takenoko.rb
@@ -48,10 +48,10 @@ module Takenoko
   mattr_accessor :folder_id
   @@folder_id = nil
 
-
   require 'takenoko/exporter'
   require 'takenoko/google_client'
   require 'takenoko/attach_helper'
+  require 'takenoko/mapping_generator'
 
   def config
     yield self
@@ -62,14 +62,8 @@ module Takenoko
     return @@google_client
   end
 
-  def check_config
-    raise "Must specify mapping_file or sheet_id" unless (@@mapping_file || @@sheet_id)
-    raise "file not found:#{@@mapping_file}" if @@mapping_file && !::File.exist?(@@mapping_file)
-    return true
-  end
-
   def mapping_config
-    (@@always_reload && generate_base_config) || @@mapping_config || generate_base_config
+    MappingGenerator.generate
   end
 
   def table_config(table_name)
@@ -109,72 +103,6 @@ module Takenoko
       end
     end
   end
-
-  def generate_base_config
-    return unless check_config
-
-    conf = HashWithIndifferentAccess.new
-
-    if @@sheet_id
-      sheet_conf = HashWithIndifferentAccess.new({tables: {}})
-      google_client.spreadsheet.worksheets.each do |ws|
-        next if ws.title.match(/\s*#.*/)
-        sheet_conf[:tables][ws.title] = {
-          worksheet_id: ws.gid,
-          worksheet: ws.title
-        }
-      end
-      conf.deep_merge!(sheet_conf)
-    end
-
-    if @@mapping_file
-      file_conf = YAML.load_file(@@mapping_file).with_indifferent_access
-      raise "tables not exists" if file_conf[:tables].blank?
-      conf.deep_merge!(file_conf)
-    end
-
-    conf[:tables].compact!
-    conf[:tables].each do |t, v|
-      unless conf[:tables][t]
-        conf[:tables][t] = Hash.new
-      end
-      table = conf[:tables][t]
-      raise "#{sheet_id} cannot be blank" unless table[:sheet_id] ||= @@sheet_id
-      table[:worksheet] = t if table[:worksheet].blank? && table[:worksheet_id].blank?
-      table[:find_column] = table[:find_column] || :id
-      table_name = table[:table_name] = t.pluralize if table[:table_name].blank?
-      table[:class_name] = table[:class_name] || (table_name && table_name.singularize.camelize) || table[:table_name].singularize.camelize
-      [
-        :allow_overwrite,
-        :truncate_all_data,
-        :file_extension,
-        :export_file_location,
-        :enable_postprocess,
-        :postprocess_class,
-        :folder_id
-      ].each do |f|
-        table[f] = class_variable_get("@@" + f.to_s) unless table.key?(f)
-      end
-
-      if table[:enable_postprocess] && table[:postprocess_class].blank?
-        table[:postprocess_class] = table[:class_name]
-      end
-
-      raise "Not support file extension: #{table[:file_extension]}" unless SUPPORTED_FILE_EXT.include?(table[:file_extension])
-
-      table[:download_location] = table[:download_location] || (@@download_location  && (@@download_location +"/"+table[:table_name]))
-      if attach_files = table[:attach_files].present?
-        attach_files = table[:attach_files]
-        attach_files = attach_files.map do |col|
-          raise "column_name must be set" unless col[:column_name]
-          col[:download_location] = col[:download_location].present? ? table[:download_location] + "/" + col[:download_location] : table[:download_location]
-          raise "folder_id should be set" unless col[:folder_id] = col[:folder_id] || table[:folder_id]
-        end
-      end
-    end
-    @@mapping_config = conf
-  end
-  alias reload_config! generate_base_config
 
   class Railtie < Rails::Railtie
     railtie_name :takenoko
---
+++ lib/takenoko/mapping_generator.rb
@@ -0,0 +1,113 @@
+module Takenoko
+  module MappingGenerator
+    extend self
+    @@mapping_conf = nil
+    ALLOWED_OVERWRITE_FIELD = [
+      :allow_overwrite,
+      :truncate_all_data,
+      :file_extension,
+      :export_file_location,
+      :enable_postprocess,
+      :postprocess_class,
+      :folder_id
+    ]
+
+    FILTER = [
+      [:set_worksheet, ->(name, table) {
+        table[:worksheet] = name if table[:worksheet].blank? && table[:worksheet_id].blank?
+      }],
+
+      [:set_sheetid, ->(name, table){
+        raise "#{sheet_id} cannot be blank" unless table[:sheet_id] ||= Takenoko.sheet_id
+      }],
+
+      [:set_find_column, ->(name, table){
+        table[:find_column] ||= :id
+      }],
+
+      [:set_tablename, ->(name, table){
+        table[:table_name] = name.pluralize if table[:table_name].blank?
+      }],
+
+      [:set_class_name, ->(name, table){
+        table[:class_name] ||= table[:table_name] && table[:table_name].singularize.camelize
+      }],
+
+      [:overwrite_global_config, ->(name, table){
+        ALLOWED_OVERWRITE_FIELD.each do |f|
+          table[f] = Takenoko.public_send(f) unless table.key?(f)
+        end
+      }],
+
+      [:set_postprocess_class, ->(name, table){
+        table[:postprocess_class] ||= table[:class_name]
+      }],
+
+      [:check_file_extension, ->(name, table){
+        raise "Not support file extension: #{table[:file_extension]}" unless SUPPORTED_FILE_EXT.include?(table[:file_extension])
+      }],
+
+      [:set_download_location, ->(name, table){
+        table[:download_location] ||= Takenoko.download_location && (Takenoko.download_location + "/" + table[:table_name])
+      }],
+
+      [:set_attach_files, ->(name, table){
+        if (attach_files = table[:attach_files]).present?
+          attach_files = attach_files.map do |col|
+            raise "column_name must be set" unless col[:column_name]
+            raise "folder_id should be set" unless col[:folder_id] ||= table[:folder_id]
+            col[:download_location] ||= col[:download_location].present? ? table[:download_location] + "/" + col[:download_location] : table[:download_location]
+          end
+        end
+      }],
+    ]
+
+    def generate
+      return @@mapping_conf if Takenoko.allow_overwrite && @@mapping_conf
+      @@mapping_conf = base_config
+      @@mapping_conf[:tables].each do |name, table|
+        FILTER.each do |f|
+          f[1].call(name, table)
+        end
+      end
+      return @@mapping_conf
+    end
+
+    private
+
+    def check_config
+      raise "Must specify mapping_file or sheet_id" unless (Takenoko.mapping_file || Takenoko.sheet_id)
+      raise "file not found:#{Takenoko.mapping_file}" if Takenoko.mapping_file && !::File.exist?(Takenoko.mapping_file)
+      return true
+    end
+
+    def spread_sheet_config
+      sheet_conf = HashWithIndifferentAccess.new({tables: {}})
+      return sheet_conf unless Takenoko.sheet_id
+      Takenoko.google_client.spreadsheet.worksheets.each do |ws|
+        next if ws.title.match(/\s*#.*/)
+        sheet_conf[:tables][ws.title] = {
+          worksheet_id: ws.gid,
+          worksheet: ws.title
+        }
+      end
+      sheet_conf
+    end
+
+    def mapping_file_config
+      return {} unless Takenoko.mapping_file
+      file_conf = YAML.load_file(Takenoko.mapping_file).with_indifferent_access
+      raise "tables not exists" if file_conf[:tables].blank?
+      file_conf
+    end
+
+    def base_config
+      check_config
+      spread_sheet_config
+      conf = spread_sheet_config.deep_merge(mapping_file_config).with_indifferent_access
+      conf[:tables].compact!
+      conf
+    end
+
+  end
+end
